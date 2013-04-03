dd1bcd917b33255a57287942512a26bd73130b51
Ed Halferty <ed@ed-K53E.(none)>
2013-04-03 18:15:35 -0400

    Added devise gem, set up the app as mentioned in the devise install notes.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdeasManager::Application.config.secret_token = '9922fa443fe6304d1ee4114e42eea1ff089745442e382fdb9102d1e1db77d4e247353c4e08ec5e2f721de218654f1523df4241195aac209ff36691fb108700a4'
